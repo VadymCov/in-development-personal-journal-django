c3e15196b5e5ef11ab384e6052de04d718a9c25f
vadym <vadymkravtcsov@gmail.com>
2025-11-24 23:53:27 +0200

    fix: correct formatting and add missing newline in EntrySerializers

--- diary_project/diary/serializers.py
+++ diary_project/diary/serializers.py
@@ -13,7 +13,7 @@ class EntrySerializers(serializers.ModelSerializer):
     tags = TagSerializers(many=True, read_only=True)
     tag_ids = serializers.PrimaryKeyRelatedField(
         queryset=Tag.objects.all(),
-        many=True,
+        many=True,  
         write_only=True,
         source="tags"
     )
@@ -21,7 +21,7 @@ class EntrySerializers(serializers.ModelSerializer):
         source="author.username",
         read_only=True
         )
-    
+
     class Meta:
         model = Entry
         field = [
@@ -34,9 +34,16 @@ class EntrySerializers(serializers.ModelSerializer):
             "update",
             "author_username",
             ]
-        read_only_field =[
+        read_only_field = [
             "id",
             "created",
             "update",
             "author_username"
         ]
+
+    def create(self, validate_data):
+        tags = validate_data.pop("tags", [])
+        entry = Entry.objects.create(**validate_data)
+        entry.tags.set(tags)
+        return entry
+    
